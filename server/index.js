c187afa35118c1c4f0541a02c8353ad505a1f01a
Emily Morehouse <emily@cuttlesoft.com>
2019-06-29 22:51:44 -0600

    [#166998704] 👕 (Base) Format all files (#2)

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const prettyHost = customHost || 'localhost'
 
 // use the gzipped bundle
 app.get('*.js', (req, res, next) => {
-  req.url = req.url + '.gz'; // eslint-disable-line
+  req.url = req.url + '.gz' // eslint-disable-line
   res.set('Content-Encoding', 'gzip')
   next()
 })
