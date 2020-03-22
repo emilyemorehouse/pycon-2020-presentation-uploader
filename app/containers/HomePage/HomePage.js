17b35dd78d23614350323bfb312c450fbf3b55f6
Emily Morehouse <emily@cuttlesoft.com>
2020-03-22 14:19:38 -0600

    [LIVE-14] 🔄 (HomePage) Remove boilerplate code; Add simple buttons to access Login and Register

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -1,20 +1,16 @@
-import React, { useContext, useEffect, useState, memo } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
 import { FormattedMessage } from 'react-intl'
-import { observer } from 'mobx-react'
+import PropTypes from 'prop-types'
 
 // Components
-import { Header } from 'components/Header'
-import { ReposList } from 'components/ReposList'
+import { Box } from 'components/Box'
+import { Button } from 'components/Button'
+import { Heading } from 'components/Heading'
+import { Text } from 'components/Text'
 
 // Messages
 import messages from './HomePage.messages'
-
-// Styles
-import { AtPrefix, CenteredSection, Form, Input, Section } from './HomePage.styles'
-
-// Context / Stores
-import { GitHubStoreContext } from '../../stores/GithubStore'
 
 /*
  * HomePage
@@ -22,90 +18,34 @@ import { GitHubStoreContext } from '../../stores/GithubStore'
  * The HomePage renders:
  * - The page title and metadata using Helmet
  * - The page header and description
- * - The search input and repository listing for a given user
  *
- * A user can:
- * - Show Github repositories by username
- *   - When a user inputs a username into the input field then hits the
- *     enter key, the form is submitted and search results are retrieved
- *   - Repositories are displayed using the ReposList component, which handles
- *     loading and error messages
  */
-export const HomePage = observer(() => {
-  const gitHubStore = useContext(GitHubStoreContext)
-  const [searchTerm, setSearchTerm] = useState('')
+export const HomePage = ({ history }) => (
+  <>
+    <Helmet>
+      <title>Home Page</title>
+      <meta name="description" content="A React.js Boilerplate application homepage" />
+    </Helmet>
 
-  /**
-   * Prevent the default behavior from occuring (for example, a page refresh),
-   * then initiate fetching repositories based on the current `searchTerm`
-   *
-   * @param {object} evt The sythetic event from firing the function via
-   *   `onSubmit`
-   */
-  const onSubmitForm = evt => {
-    evt.preventDefault()
-    gitHubStore.getRepos(searchTerm)
-  }
+    <Box align="center">
+      <Heading level="2" color="brand">
+        <FormattedMessage {...messages.startProjectHeader} />
+      </Heading>
 
-  /**
-   * When the initial username from the store is not null, set the local state
-   * for `searchTerm` to the provided username and initiate fetching
-   * repositories
-   */
-  useEffect(() => {
-    if (gitHubStore.currentUser && gitHubStore.currentUser.trim().length > 0) {
-      setSearchTerm(gitHubStore.currentUser)
-      gitHubStore.getRepos(gitHubStore.currentUser)
-    }
-  }, [])
+      <Text>
+        <FormattedMessage {...messages.startProjectMessage} />
+      </Text>
 
-  return (
-    <article>
-      <Helmet>
-        <title>Home Page</title>
-        <meta name="description" content="A React.js Boilerplate application homepage" />
-      </Helmet>
+      <Box gap="small" direction="row" pad="large">
+        <Button primary label="Login" onClick={() => history.push('/login')} />
+        <Button primary label="Register" onClick={() => history.push('/register')} />
+      </Box>
+    </Box>
+  </>
+)
 
-      <CenteredSection>
-        <Header level="2" color="brand">
-          <FormattedMessage {...messages.startProjectHeader} />
-        </Header>
-        <p>
-          <FormattedMessage {...messages.startProjectMessage} />
-        </p>
-      </CenteredSection>
+HomePage.propTypes = {
+  history: PropTypes.object,
+}
 
-      <Section>
-        <Header level="2">
-          <FormattedMessage {...messages.trymeHeader} />
-        </Header>
-
-        <Form data-testid="search-form" onSubmit={onSubmitForm}>
-          <label htmlFor="searchTerm">
-            <FormattedMessage {...messages.trymeMessage} />
-
-            <AtPrefix>
-              <FormattedMessage {...messages.trymeAtPrefix} />
-            </AtPrefix>
-
-            <Input
-              id="searchTerm"
-              type="text"
-              placeholder="mxstbr"
-              value={searchTerm}
-              onChange={e => setSearchTerm(e.target.value)}
-            />
-          </label>
-        </Form>
-
-        <ReposList
-          loading={gitHubStore.isLoading}
-          error={gitHubStore.hasError}
-          repos={gitHubStore.repos}
-        />
-      </Section>
-    </article>
-  )
-})
-
-export default memo(HomePage)
+export default HomePage
--- app/containers/HomePage/HomePage.messages.js
+++ app/containers/HomePage/HomePage.messages.js
@@ -10,23 +10,10 @@ export const scope = 'boilerplate.containers.HomePage'
 export default defineMessages({
   startProjectHeader: {
     id: `${scope}.start_project.header`,
-    defaultMessage: 'Start your next react project in seconds',
+    defaultMessage: 'LiveRider',
   },
   startProjectMessage: {
     id: `${scope}.start_project.message`,
-    defaultMessage:
-      'A highly scalable, offline-first foundation with the best DX and a focus on performance and best practices',
-  },
-  trymeHeader: {
-    id: `${scope}.tryme.header`,
-    defaultMessage: 'Try me!',
-  },
-  trymeMessage: {
-    id: `${scope}.tryme.message`,
-    defaultMessage: 'Show Github repositories by',
-  },
-  trymeAtPrefix: {
-    id: `${scope}.tryme.atPrefix`,
-    defaultMessage: '@',
+    defaultMessage: 'The link between music venues, touring artists, and crew.',
   },
 })
