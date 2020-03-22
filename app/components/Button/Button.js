c666ff7825481db1487631ea227528caef0780ac
Emily Morehouse <emily@cuttlesoft.com>
2020-03-22 13:19:04 -0600

    [LIVE-14] 🔄 (Button) Simplify wrapper component with prop drilling; add default font family and styles

--- app/components/Button/Button.js
+++ app/components/Button/Button.js
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import { Button } from 'grommet'
+import { Button as GrommetButton } from 'grommet'
+import styled from 'styled-components'
 
 import { doc } from './Button.doc'
 import helpers from '../../utils/helpers'
+
+const StyledGrommetButton = styled(GrommetButton)`
+  font-weight: bold;
+  font-family: 'Roboto Mono';
+`
 
 /**
  *
@@ -11,60 +17,12 @@ import helpers from '../../utils/helpers'
  *
  * A common button
  */
-function createButton({
-  a11yTitle,
-  active,
-  alignSelf,
-  as,
-  color,
-  disabled,
-  fill,
-  focusIndicator,
-  gap,
-  gridArea,
-  hoverIndicator,
-  href,
-  icon,
-  margin,
-  onClick,
-  plain,
-  primary,
-  reverse,
-  target,
-  title,
-  type,
-  ...rest
-}) {
-  return (
-    <Button
-      a11yTitle={a11yTitle}
-      active={active}
-      alignSelf={alignSelf}
-      as={as}
-      color={color}
-      disabled={disabled}
-      fill={fill}
-      focusIndicator={focusIndicator}
-      gap={gap}
-      gridArea={gridArea}
-      hoverIndicator={hoverIndicator}
-      href={href}
-      icon={icon}
-      label={title}
-      margin={margin}
-      onClick={onClick}
-      plain={plain}
-      primary={primary}
-      reverse={reverse}
-      target={target}
-      type={type}
-      {...rest}
-    />
-  )
+function Button({ ...rest }) {
+  return <StyledGrommetButton {...rest} />
 }
 
 export default helpers.createWithDoc({
   envName: process.env.NODE_ENV,
   docFunction: doc,
-  component: createButton,
+  component: Button,
 })
