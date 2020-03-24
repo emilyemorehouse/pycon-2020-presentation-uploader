3441497245c998066326c2ed33a42571f0d8d4bf
Emily Morehouse <emily@cuttlesoft.com>
2020-03-23 22:06:15 -0600

    [LIVE-14] 🔄 (Dashboard) Hook up `New` buttons to DemoForm in a PopoutForm

--- app/containers/Dashboard/Dashboard.js
+++ app/containers/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from 'grommet' /** @todo replace with wrapper component */
 import { observer } from 'mobx-react'
 
@@ -6,72 +6,91 @@ import { observer } from 'mobx-react'
 import { Box } from 'components/Box'
 import { Button } from 'components/Button'
 import { Grid } from 'components/Grid'
+import { FormField } from 'components/Form'
 import { Heading } from 'components/Heading'
 import { Text } from 'components/Text'
+import { PopoutForm } from 'components/PopoutForm'
 
 // Helpers
 import { GRID_COLUMNS, GRID_ROWS, GRID_AREAS } from './Dashboard.constants'
+
+const DemoForm = (
+  <>
+    <FormField label="Email" name="email" required />
+
+    <FormField label="Password" name="password" required />
+  </>
+)
 
 /**
  *
  * Dashboard
  *
  */
-const Dashboard = observer(() => (
-  <>
-    <Heading level="2" pad="small">
-      Dashboard
-    </Heading>
+const Dashboard = observer(() => {
+  const [showForm, setShowForm] = useState(undefined)
 
-    <Grid
-      areas={GRID_AREAS}
-      columns={GRID_COLUMNS}
-      fill="vertical"
-      gap="small"
-      pad="small"
-      justifyContent="center"
-      rows={GRID_ROWS}
-    >
-      <Box gridArea="shows">
-        <Heading margin="small" level={4}>
-          Shows
-        </Heading>
+  return (
+    <>
+      <PopoutForm form={DemoForm} showForm={showForm} setShowForm={setShowForm} />
 
-        <Header background="light-5" pad="medium">
-          <Text>Sort</Text>
-          <Button primary label="Add New"></Button>
-        </Header>
+      <Heading level="2" pad="small">
+        Dashboard
+      </Heading>
 
-        <Box fill border={{ color: 'light-5', size: 'thin' }}></Box>
-      </Box>
+      <Grid
+        areas={GRID_AREAS}
+        columns={GRID_COLUMNS}
+        fill="vertical"
+        gap="small"
+        pad="small"
+        justifyContent="center"
+        rows={GRID_ROWS}
+      >
+        <Box gridArea="shows">
+          <Heading margin="small" level={4}>
+            Shows
+          </Heading>
 
-      <Box gridArea="todos">
-        <Heading margin="small" level={4}>
-          To Dos
-        </Heading>
+          <Header background="light-5" pad="medium">
+            <Text>Sort</Text>
 
-        <Header background="light-5" pad="medium">
-          <Text>Sort</Text>
-          <Button primary label="Add New"></Button>
-        </Header>
+            <Button primary label="Add New" onClick={() => setShowForm(true)}></Button>
+          </Header>
 
-        <Box fill border={{ color: 'light-5', size: 'thin' }}></Box>
-      </Box>
+          <Box fill border={{ color: 'light-5', size: 'thin' }}></Box>
+        </Box>
 
-      <Box gridArea="artist-venues">
-        <Heading margin="small" level={4}>
-          Artists & Venues
-        </Heading>
+        <Box gridArea="todos">
+          <Heading margin="small" level={4}>
+            To Dos
+          </Heading>
 
-        <Header background="light-5" pad="medium">
-          <Text>Sort</Text>
-          <Button primary label="Add New"></Button>
-        </Header>
+          <Header background="light-5" pad="medium">
+            <Text>Sort</Text>
 
-        <Box fill border={{ color: 'light-5', size: 'thin' }}></Box>
-      </Box>
-    </Grid>
-  </>
-))
+            <Button primary label="Add New" onClick={() => setShowForm(true)}></Button>
+          </Header>
+
+          <Box fill border={{ color: 'light-5', size: 'thin' }}></Box>
+        </Box>
+
+        <Box gridArea="artist-venues">
+          <Heading margin="small" level={4}>
+            Artists & Venues
+          </Heading>
+
+          <Header background="light-5" pad="medium">
+            <Text>Sort</Text>
+
+            <Button primary label="Add New" onClick={() => setShowForm(true)}></Button>
+          </Header>
+
+          <Box fill border={{ color: 'light-5', size: 'thin' }}></Box>
+        </Box>
+      </Grid>
+    </>
+  )
+})
 
 export default Dashboard
